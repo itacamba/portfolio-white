4cab0f8497ab235ef92b85cb47be24d2306c4c03
itacamba <daniela.sahagun03@gmail.com>
2020-11-02 23:58:11 -0500

    main works responsive till 250px mobile

--- src/components/Main.js
+++ src/components/Main.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import Carla from "./images/carla-round-portrait.png"
+import "../App.css"
 import "./css/Main.css"
 
 const Main = () => {
@@ -10,8 +11,8 @@ const Main = () => {
             </div>
             <h3>HELLO, MY NAME IS</h3>
             <h1>Carla Sahagun</h1>
-            <p>I am a software developer based in Herndon, VA.<br></br>
-                I specialize in building web applications that<br></br>
+            <p>I am a software developer based in Herndon, VA.
+                I specialize in building web applications that
                 deliver exceptional user experiences.
             </p>
             <a href=""><i class="fas fa-chevron-down"></i></a>
